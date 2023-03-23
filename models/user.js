a5ebfcef6d95c6694b83e8f9d31bffd5bfd4ab74
pablomesny <pablomesny@gmail.com>
2023-03-23 20:19:04 -0300

    format changes

--- models/user.js
+++ models/user.js
@@ -2,42 +2,42 @@ const { DataTypes } = require('sequelize');
 const db = require('../database/connection');
 
 const User = db.define(
-  'User',
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      autoIncrement: true,
-      primaryKey: true
+    'User',
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        state: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: true
+        }
     },
-    username: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true
-    },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    state: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: true
+    {
+        timestamps: false
     }
-  },
-  {
-    timestamps: false
-  }
 );
 
 User.sync()
-  .then(() => {
-    console.log('Users table created successfully');
-  })
-  .catch(error => {
-    console.log('Error creating Users table', error);
-  });
+    .then(() => {
+        console.log('Users table created successfully');
+    })
+    .catch(error => {
+        console.log('Error creating Users table', error);
+    });
 
 module.exports = User;
