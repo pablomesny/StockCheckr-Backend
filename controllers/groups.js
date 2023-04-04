18ce4045ccad99a786677fc7cab37ccceee0e27f
pablomesny <pablomesny@gmail.com>
2023-04-04 08:12:47 -0300

    getGroups controller hotfix

--- controllers/groups.js
+++ controllers/groups.js
@@ -28,11 +28,11 @@ const createGroup = async( req = request, res = response ) => {
 
 const getGroups = async( req = request, res = response ) => {
 
-    const { userId } = req.params;
+    const { userId: id } = req.params;
     const { from = 0, limit = 5 } = req.query;
 
     try {
-        const { count, rows } = await Group.findAndCountAll({ where: { created_by: userId },limit, offset: from });
+        const { count, rows } = await Group.findAndCountAll({ where: { created_by: id },limit, offset: from });
     
         res.json({
             ok: true,
