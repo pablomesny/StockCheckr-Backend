9bf1aedbd103c9fc92c8f443f006b195e6ce9e8f
pablomesny <pablomesny@gmail.com>
2023-04-17 08:53:45 -0300

    stock reduction added into createSale controller

--- controllers/sales.js
+++ controllers/sales.js
@@ -1,5 +1,6 @@
 const { request, response } = require("express");
 const Sale = require("../models/sale");
+const { Product } = require("../models");
 
 const getSales = async( req = request, res = response ) => {
 
@@ -26,18 +27,28 @@ const getSales = async( req = request, res = response ) => {
 const createSale = async( req = request, res = response ) => {
 
     const { userId: id } = req.params;
-    const { product, amount = 1 } = req.body;
+    const { product: productId, amount = 1 } = req.body;
 
     try {
-        const sale = await Sale.build({ product, amount, client_id: id });
+        const sale = await Sale.build({ productId, amount, client_id: id });
+        const product = await Product.findByPk( productId );
 
+        if( product.stock < amount ) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'Insufficient stock'
+            })
+        }
+
+        const productStock = product.stock - amount;
+
+        await Product.update({ stock: productStock }, { where: productId });
         await sale.save();
 
         res.status(201).json({
             ok: true,
             sale
         })
-
 
     } catch (error) {
         console.log(error);
