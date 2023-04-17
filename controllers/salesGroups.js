95814007592ff44ffaa73b04eddf0953ff9cf7ce
pablomesny <pablomesny@gmail.com>
2023-04-17 15:28:49 -0300

    createSalesGroup controller added

--- controllers/salesGroups.js
+++ controllers/salesGroups.js
@@ -23,7 +23,32 @@ const getSalesGroups = async( req = request, res = response ) => {
     }
 }
 
+const createSalesGroup = async( req = request, res = response ) => {
+
+    const { id } = req.user;
+    const { sales, totalPrice } = req.body;
+
+    try {
+        const salesGroup = await SalesGroup.build({ sales, totalPrice, created_by: id });
+        await salesGroup.save();
+
+        res.status(201).json({
+            ok: true,
+            salesGroup
+        })
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Error creating sales group'
+        })
+    }
+
+}
+
 
 module.exports = {
-    getSalesGroups
+    getSalesGroups,
+    createSalesGroup
 }
--- routes/salesGroups.js
+++ routes/salesGroups.js
@@ -1,9 +1,18 @@
 const { Router } = require('express');
-const { getSalesGroups } = require('../controllers/salesGroups');
+const { getSalesGroups, createSalesGroup } = require('../controllers/salesGroups');
+const validateJWT = require('../middlewares/validate-jwt');
+const validateFields = require('../middlewares/validate-fields');
 
 const router = new Router();
 
 router.get( '/', getSalesGroups );
+
+router.post( '/', [
+    validateJWT,
+    check( 'sales', 'Sales are mandatory' ).not().isEmpty(),
+    check( 'totalPrice', 'Total price is mandatory' ).not().isEmpty(),
+    validateFields
+], createSalesGroup );
 
 
 module.exports = router;
